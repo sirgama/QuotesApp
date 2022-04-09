5ea435f065c0412cf8e8c4621c4b6b0d0a854f19
sirgama <sirengogamaliel@gmail.com>
2022-04-09 14:35:13 +0300

    ft: add a hghlight directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeroComponent } from './hero/hero.component';
 import { QuotesAreaComponent } from './quotes-area/quotes-area.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { QuotesAreaComponent } from './quotes-area/quotes-area.component';
     NavbarComponent,
     FooterComponent,
     HeroComponent,
-    QuotesAreaComponent
+    QuotesAreaComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
