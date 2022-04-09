ced21fbdef211a080eac474876a2c6c1d55902f2
sirgama <sirengogamaliel@gmail.com>
2022-04-09 12:24:28 +0300

    ft: add quotes interpolation

--- src/app/quotes-area/quotes-area.component.ts
+++ src/app/quotes-area/quotes-area.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import  { }
+import { Quotearea } from '../quotearea';
 @Component({
   selector: 'app-quotes-area',
   templateUrl: './quotes-area.component.html',
@@ -7,6 +7,12 @@ import  { }
 })
 export class QuotesAreaComponent implements OnInit {
 
+  quotesall: Quotearea[] = [
+    new Quotearea(1, "Sample Quote", 0,0),
+    new Quotearea(2, "Second Quote", 0,0),
+    new Quotearea(3, "Another Quote", 0,0),
+    new Quotearea(4, "Final sample  Quote", 0,0),
+  ]
   constructor() { }
 
   ngOnInit(): void {
