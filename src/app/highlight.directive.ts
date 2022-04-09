6ba545b5f08a8ad48bb2edbcdf91e301383f1dcd
sirgama <sirengogamaliel@gmail.com>
2022-04-09 15:28:20 +0300

    ft: add highlight on highest upvote

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private select: ElementRef) {
+    select.nativeElement.style.background = "#e9e767 "
+  }
 
 }
--- src/app/quotes-area/quotes-area.component.ts
+++ src/app/quotes-area/quotes-area.component.ts
@@ -9,13 +9,16 @@ export class QuotesAreaComponent implements OnInit {
 
   quotesall: Quotearea[] = [
     new Quotearea(1, "Sample Quote", 0,0),
-    new Quotearea(2, "Second Quote", 0,0),
+    new Quotearea(2, "Second Quote", 1,0),
     new Quotearea(3, "Another Quote", 0,0),
-    new Quotearea(4, "Final sample  Quote", 0,0),
+    new Quotearea(4, "Final sample  Quote", 5,8),
   ]
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+var maximum = Math.max(...arr);
